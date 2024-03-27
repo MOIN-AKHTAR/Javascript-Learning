829f7b46b0199915fe652aee6fa3e93a9d734222
MOIN-AKHTAR <moinakhter179@gmail.com>
2024-03-27 22:46:23 +0500

    1st and 2nd class

---
+++ class1.js
@@ -0,0 +1,30 @@
+
+// var, let, const
+var _var;
+_var="VAR VALUE"
+console.log(_var)
+
+_var= "ANOTHER _VAR VALUE"
+console.log(_var)
+
+let _let;
+_let = "LET VALUE"
+console.log(_let)
+
+const _const= "CONSTaddition VALUE"
+console.log(_const)
+
+// string,boolean,number
+//  `  This is called template string
+// typeof data   =====> It tells about what is the type of variable.
+let data=false;
+
+let num=12;
+
+const name= "Moin"
+const city= "ISLAMABAD"
+const description= `MY name is ${name} and i live in ${city}`
+
+console.log(description)
+console.log(typeof description)
+
---
+++ class2.js
@@ -0,0 +1,24 @@
+// FUNCTION:-
+
+// Declaring Function
+function sum(){
+    console.log("HELLO WORLD")
+}
+// Declaring Function
+const addition=function(a,b){
+  console.log(a-b)
+}
+
+// CALLING FUNCTION
+sum()
+// CALLING FUNCTION
+addition(1,2)
+
+const log=function(){
+    console.log("HELLO EVERYONE")
+}
+
+log()
+console.log("HELLO EVERYONE")
+
+
