22c1f243800aff6f256afbe24e26cea61f70308c
MOIN-AKHTAR <moinakhter179@gmail.com>
2024-03-31 22:35:33 +0500

    Worked on logical and conditional operators

---
+++ class5.js
@@ -0,0 +1,35 @@
+// LOGICAL OPERATOR >,<,>=,<=,==,===,!=,!==
+// CONDITIONAL OPERATOR ! || &&
+// >  Greater Operator Operator
+// < Less Operator Operator
+// >=Greater Than Equal Operator
+// <= Less Than Equal Operator
+// ==,===  Equality Operator
+// !=,!==  Not Equality Operator
+//  && And Operator
+//  || Or Operator
+console.log(3>4)
+console.log(3<4)
+console.log(3<=4)
+console.log(3<=3)
+console.log(3>=4)
+console.log(4>=4)
+console.log(3==4)
+console.log(3===4)
+console.log(3=="3")
+console.log(3==="3")
+console.log(3!=2)
+console.log(3!="2")
+console.log(3!==2>2)
+console.log(3!=="3")
+
+
+// true =1 , false=0
+console.log(false+1)
+console.log("false"+"2")
+console.log("werty"+"werty")
+console.log(3>2>1)
+console.log(1<2<3)
+
+console.log(3>2&&4>5)
+console.log(3>2||4>5)
