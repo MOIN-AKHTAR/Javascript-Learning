58cb12fae9afa5b0f4bbb90f62371b3118199703
MOIN-AKHTAR <moinakhter179@gmail.com>
2024-04-02 22:39:07 +0500

    Worked on loops

---
+++ class7.js
@@ -0,0 +1,24 @@
+// LOOPS
+
+// while
+let i=11;
+while (i<=5) {
+    console.log("HELLO WORLD FROM WHILE")
+    i++;
+}
+
+// do-while
+let j=11;
+do {
+    console.log("HELLO WORLD FROM DO WHILE")
+    j++;
+} while (j<=5);
+
+
+// for
+for (let k = 2; k <= 10; k+=2) {
+    console.log(k)
+}
+
+console.log("ok")
+
